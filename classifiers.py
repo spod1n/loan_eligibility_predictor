b942e8cc12be8217770c4a4c584b9b9b02e8ed9e
spod1n <k.spodin@gmail.com>
2024-04-29 07:31:08 +0300

    add new models, create diagrams, add new metrics

--- classifiers.py
+++ classifiers.py
@@ -1,10 +1,15 @@
 import joblib
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import (accuracy_score, classification_report, confusion_matrix,
+                             r2_score, mean_absolute_percentage_error)
 
 from dataset import GetDataset
 
@@ -12,7 +17,8 @@ from dataset import GetDataset
 class LoanClassifier(GetDataset):
     def __init__(self, classifiers_dict):
         super().__init__()
-        self.pipelines = self._build_pipeline(classifiers_dict)
+        self.classifiers_dict = classifiers_dict
+        self.pipelines = self._build_pipeline()
         self.param_grid = {
             'RandomForest': {
                 'classifier__n_estimators': [100, 200, 300],
@@ -24,21 +30,36 @@ class LoanClassifier(GetDataset):
                 'classifier__weights': ['uniform', 'distance'],
                 'classifier__algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
                 'classifier__p': [1, 2]
+            },
+            'GradientBoosting': {
+                'classifier__n_estimators': [50, 100, 150],
+                'classifier__learning_rate': [0.01, 0.1, 0.5],
+                'classifier__max_depth': [3, 5, 7],
+                'classifier__subsample': [0.8, 1.0],
+                'classifier__min_samples_split': [2, 5, 10],
+                'classifier__min_samples_leaf': [1, 2, 4]
+            },
+            'DecisionTree': {
+                'classifier__criterion': ['gini', 'entropy'],
+                'classifier__splitter': ['best', 'random'],
+                'classifier__max_depth': [None, 10, 20],
+                'classifier__min_samples_split': [2, 5, 10],
+                'classifier__min_samples_leaf': [1, 2, 4]
             }
         }
 
-    def _build_pipeline(self, classifiers_dict: dict) -> {Pipeline}:
+    def _build_pipeline(self) -> {Pipeline}:
         """
         Build the machine learning pipeline.
         Preprocesses the data, sets up feature transformations, and defines the classifier.
         """
         # Separate features and target variable
-        x = self.dataset.drop('Loan_Status', axis=1)
+        self.x = self.dataset.drop('Loan_Status', axis=1)
         y = self.dataset['Loan_Status']
 
         # Split data into training and test sets
         (self.x_train, self.x_test,
-         self.y_train, self.y_test) = train_test_split(x, y, test_size=0.2, random_state=42)
+         self.y_train, self.y_test) = train_test_split(self.x, y, test_size=0.2, random_state=42)
 
         # Use it to transform the training and test target variables to number
         label_encoder = LabelEncoder()
@@ -46,11 +67,11 @@ class LoanClassifier(GetDataset):
         self.y_test = label_encoder.transform(self.y_test)
 
         # Pre-processing pipeline for numeric features
-        numeric_features = x.select_dtypes(include=['number']).columns.tolist()
+        numeric_features = self.x.select_dtypes(include=['number']).columns.tolist()
         numeric_transformer = Pipeline(steps=[('scaler', StandardScaler())])
 
         # Pre-processing pipeline for categorical features
-        categorical_features = x.select_dtypes(include=['object']).columns.tolist()
+        categorical_features = self.x.select_dtypes(include=['object']).columns.tolist()
         categorical_transformer = Pipeline(steps=[('onehot', OneHotEncoder())])
 
         # Combine pre-processing pipeline
@@ -58,7 +79,7 @@ class LoanClassifier(GetDataset):
                                                        ('cat', categorical_transformer, categorical_features)])
         # Create the full pipelines with specified classifier
         pipelines = dict()
-        for name, model in classifiers_dict.items():
+        for name, model in self.classifiers_dict.items():
             pipelines[name] = Pipeline(steps=[('preprocessor', preprocessor), ('classifier', model)])
 
         return pipelines
@@ -72,12 +93,27 @@ class LoanClassifier(GetDataset):
             cv_scores = cross_val_score(pipeline, self.x_train, self.y_train, cv=5)
             print(f'Cross-validation scores for {name}: {cv_scores.mean()}', cv_scores, sep='\n', end='\n\n')
 
+            # Determining the importance of features
+            if name != 'KNeighbors':    # the 'KNeighbors' model does not support the importance of features
+                feature_importances = pipeline.named_steps['classifier'].feature_importances_
+                feature_importance_df = pd.DataFrame({'Feature': self.x.columns,
+                                                      'Importance': feature_importances[:len(self.x.columns)]
+                                                      })
+                sorted_feature_importance = feature_importance_df.sort_values(by='Importance', ascending=False)
+                # Visualize feature importance
+                plt.figure(figsize=(10, 6))
+                sns.barplot(x='Importance', y='Feature', data=sorted_feature_importance)
+                plt.title(f"Feature Importance '{name}'")
+                plt.xlabel('Importance')
+                plt.ylabel('Feature')
+                plt.savefig(f'diagrams/feature_importance_{name.lower()}.png')
+                plt.close()
+
     def fine_tune(self) -> None:
         """ Fine-tune the RandomForestClassifier model using GridSearchCV """
         for name, pipeline in self.pipelines.items():
             # Define the parameter grid for GridSearchCV
             if name in self.param_grid:
-                print(self.param_grid[name])
                 # Initialize GridSearchCV with the pipeline and parameter grid
                 grid_search = GridSearchCV(pipeline, self.param_grid[name], cv=5)
                 # Perform the grid search to find the best parameters
@@ -92,6 +128,7 @@ class LoanClassifier(GetDataset):
                 pipeline.set_params(**best_params)
                 pipeline.fit(self.x_train, self.y_train)
 
+                os.makedirs('pipelines') if not os.path.exists(os.path.abspath('pipelines')) else False
                 joblib.dump(pipeline, f'pipelines/{name.lower()}.pkl')
 
     def evaluate_model(self) -> None:
@@ -103,3 +140,5 @@ class LoanClassifier(GetDataset):
             print('Accuracy: ', accuracy, end='\n\n')
             print('Classification Report:', classification_report(self.y_test, y_pred), sep='\n', end='\n\n')
             print('Confusion Matrix:', confusion_matrix(self.y_test, y_pred), sep='\n', end='\n\n')
+            print('R2: ', r2_score(self.y_test, y_pred), end='\n')
+            print('MAPE: ', mean_absolute_percentage_error(self.y_test, y_pred), end='\n\n')
