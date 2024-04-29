33224f7f0e2f0e01b183495903c6939b49d14e5a
spod1n <k.spodin@gmail.com>
2024-04-29 07:32:22 +0300

    create method with create and saving visualizations

--- dataset.py
+++ dataset.py
@@ -1,4 +1,8 @@
+import os
+
 import pandas as pd
+import numpy as np
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -17,25 +21,13 @@ class GetDataset:
         print('Property Area: ', loan_df.Property_Area.unique())
         print('Dependents: ', loan_df.Dependents.unique(), end='\n\n')
 
-        # Data distribution and missing value detection
-        numeric_features = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term']
-        loan_df[numeric_features].hist(figsize=(10, 8))
-        plt.suptitle('Data distribution and missing value detection')
-        plt.tight_layout()
-        plt.savefig('diagrams/distrib_data.png')
-
-        categorical_features = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Credit_History',
-                                'Property_Area', 'Loan_Status']
-        for feature in categorical_features:
-            plt.figure(figsize=(6, 4))
-            sns.countplot(x=feature, data=loan_df)
-            plt.title(f'Distribution of {feature}')
-            plt.savefig(f'diagrams/distrib_{feature}.png')
-
         # Handle dataset
         loan_df = self._handled_dataset(loan_df)
         print('Handled dataset (first 5 rows):', loan_df.head().to_markdown(), sep='\n', end='\n\n')
         print(loan_df.dtypes.to_markdown(), end='\n\n')
+
+        # create and save diagrams
+        self.visualizations(loan_df)
 
         return loan_df
 
@@ -47,3 +39,51 @@ class GetDataset:
         df = df.dropna().reset_index(drop=True)     # Drop rows with missing values
         print('Count total NaN at each column in a DataFrame:', df.isnull().sum(), sep='\n', end='\n\n')
         return df
+
+    @staticmethod
+    def visualizations(df) -> None:
+        """ Method for additional visualizations (to cover all things) """
+        os.makedirs('diagrams') if not os.path.exists(os.path.abspath('diagrams')) else False
+        for file in os.scandir('diagrams'):
+            os.remove(file.path)
+
+        string_fields = df.select_dtypes(include=['object']).columns.tolist()
+        num_fields = df.select_dtypes(include=['number']).columns.tolist()
+
+        # Data distribution and missing value detection
+        df[num_fields].hist(figsize=(10, 8))
+        plt.suptitle('Data distribution and missing value detection')
+        plt.tight_layout()
+        plt.savefig('diagrams/distrib_data.png')
+        plt.close()
+
+        for feature in string_fields:
+            plt.figure(figsize=(6, 4))
+            sns.countplot(x=feature, data=df)
+            plt.title(f'Distribution of {feature}')
+            plt.savefig(f'diagrams/distrib_{feature.lower()}.png')
+        plt.close()
+
+        # The relationship between 'ApplicantIncome' and 'LoanAmount' with a breakdown by 'Loan_Status'
+        plt.figure(figsize=(8, 6))
+        sns.scatterplot(x='ApplicantIncome', y='LoanAmount', hue='Loan_Status', data=df)
+        plt.title('Relationship between ApplicantIncome and LoanAmount by Loan Status')
+        plt.xlabel('ApplicantIncome')
+        plt.ylabel('LoanAmount')
+        plt.legend(title='Loan Status')
+        plt.savefig('diagrams/relationship_applicantincome_loanamount.png')
+        plt.close()
+
+        # Heatmap correlation for a set of numerical data
+        numeric_data = df[num_fields]
+        matrix_corr = numeric_data.corr()
+        mask = np.triu(np.ones_like(matrix_corr, dtype=bool))
+
+        plt.subplots(figsize=(10, 3))
+        cmap = sns.diverging_palette(230, 20, as_cmap=True)
+        sns.heatmap(matrix_corr, mask=mask, cmap=cmap, annot=True)
+        plt.xticks(rotation=45)
+        plt.title('Теплова карта кореляції для набору числових даних')
+        plt.tight_layout()
+        plt.savefig('diagrams/correlation_num_data.png')
+        plt.close()
