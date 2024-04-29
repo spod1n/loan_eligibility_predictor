3849dba3d39f89659cd80f449acb0f8413ef5d44
spod1n <k.spodin@gmail.com>
2024-04-29 03:04:31 +0300

    call create pipelines from main. get dataset - from module 'classifiers'

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from dataset import GetDataset
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
+from classifiers import LoanClassifier
 
 if __name__ == '__main__':
-    GetDataset().get_dataset()
+    RANDOM_STATE = 42
+    classifiers = {
+        'RandomForest': RandomForestClassifier(RANDOM_STATE),
+        'KNeighbors': KNeighborsClassifier(RANDOM_STATE)
+    }
+
+    # Create and run the classifier
+    classifier = LoanClassifier(classifiers)
+    print(classifier)
